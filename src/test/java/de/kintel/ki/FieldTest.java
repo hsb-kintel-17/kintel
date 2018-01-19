7250b0174b799182261ddbaf1b9f80461452bbd1
Christian Zöller <noex_home@yahoo.de>
2018-01-19 01:59:27 +0100

    Added missing tests

--- src/test/java/de/kintel/ki/FieldTest.java
+++ src/test/java/de/kintel/ki/FieldTest.java
@@ -6,6 +6,9 @@ import de.kintel.ki.model.Piece;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -20,12 +23,14 @@ public class FieldTest {
 
     @Test
     public void peekHead() {
+        assertThat( field.peekHead(), is(Optional.empty()));
         field.addStein( new Piece(Player.SCHWARZ));
         assertThat( field.peekHead().orElseThrow(IllegalStateException::new).getOwner(), is(Player.SCHWARZ));
     }
 
     @Test
     public void getOwner() {
+        assertThat( field.getOwner(), is(Optional.empty()));
         field.addStein( new Piece(Player.SCHWARZ));
         field.addStein( new Piece(Player.WEISS));
         assertThat( field.getOwner().orElseThrow(IllegalStateException::new), is(Player.WEISS));
@@ -36,5 +41,32 @@ public class FieldTest {
         assertThat( field.isEmpty(), is(true));
         field.addStein( new Piece(Player.WEISS));
         assertThat( field.isEmpty(), is(false));
+    }
+
+    @Test
+    public void addStein() {
+        final Piece piece = new Piece(Player.SCHWARZ);
+        assertThat( field.isEmpty(), is(true));
+        field.addStein( piece );
+        assertThat( field.getSteine(), hasItem(piece));
+    }
+
+    @Test
+    public void pollHead() {
+        assertThat( field.pollHead(), is(Optional.empty()));
+        final Piece piece1 = new Piece(Player.SCHWARZ);
+        final Piece piece2 = new Piece(Player.WEISS);
+        field.addStein( piece1 );
+        field.addStein( piece2 );
+        assertThat( field.getSteine().size(), is(2));
+        assertThat( field.pollHead().orElseThrow(IllegalStateException::new), is(piece2));
+    }
+
+    @Test
+    public void isForbidden() {
+        Field f = new Field(true);
+        assertThat(f.isForbidden(), is(true));
+        Field f2 = new Field(false);
+        assertThat(f2.isForbidden(), is(false));
     }
 }
